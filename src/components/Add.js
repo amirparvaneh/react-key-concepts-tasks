69e0e6ec202a3a025bc195906107857b6cb94dd6
amirparvaneh <amirparvaneh138@gmail.com>
2023-08-01 23:30:44 +0330

    firstchapter4

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,32 @@
-
+import { useState } from "react";
 
 function Add() {
 
-    const  x = 2 + 4 ;
+    const[enteredNumbers,setEnteredNumbers] = useState({first:0,second:0});
 
+    function changeFirstNumber(event){
+        setEnteredNumbers((pervNumbers) => ({
+             first : +event.target.value,
+            second : pervNumbers.second,
+        }));
+}
 
-    console.log(x);
+    function changeSecondNumber(event){
+        setEnteredNumbers((pervNumbers) => ({
+            first : pervNumbers.first,
+            second : +event.target.value,
+        }));
+    }
+
+    const result = enteredNumbers.first + enteredNumbers.second;
+
+    return (
+        <p>s
+            <input type="number" onChange={changeFirstNumber}/> +{` `}
+            <input type="number" onChange={changeSecondNumber}/> +{result}
+        </p>
+    );
 
 }
+
+export default Add;
