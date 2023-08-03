37aa16c51c968c62f728f2621d54932502f719ae
amirparvaneh <amirparvaneh138@gmail.com>
2023-08-03 09:36:53 +0330

    firstchapter4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Add from './components/Add';
+import Divide from './components/Divide';
+import Multiply from './components/Multiply';
+import Subtract from './components/Subtract';
 
 function App() {
   return (
-   
-    <h1>
-      hiiii
-    </h1>
+    <>
+      <Add/>
+      <Divide/>
+      <Multiply/>
+      <Subtract/>
+    </>
   );
 }
 
